86487081a6a2e049c9ad8cf126138ee4e6765da6
4534 <yuuitirounisiyama@gmail.com>
2026-09-21 15:04:41 +0900

    Remove unused anchor click handler

--- script.js
+++ script.js
@@ -18,9 +18,3 @@ if (socialContainer && socialLinks.length) {
   });
 }
 
-document.querySelectorAll('a[href^="#"]').forEach((link) => {
-  link.addEventListener("click", () => {
-    const target = document.querySelector(link.getAttribute("href"));
-    if (target) target.setAttribute("tabindex", "-1");
-  });
-});
